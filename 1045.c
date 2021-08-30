496e4071889dda1b90cfc08eb5b0a80a184bb6b2
Al Jaziz Turja <turjo654@gmail.com>
2021-08-30 18:58:19 +0600

    Uri 1045 solve

---
+++ 1045.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+    double temp,a,b,c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if(a>=(b+c) || b>=(a+c) || c>=(b+a))
+    {
+        printf("NAO FORMA TRIANGULO\n");
+    }
+    else if((a*a)==(b*b)+(c*c) || (b*b)==(a*a)+(c*c)|| (c*c)==(b*b)+(a*a))
+    {
+        printf("TRIANGULO RETANGULO\n");
+    }
+    else if((a*a)>(b*b)+(c*c) || (b*b)>(a*a)+(c*c) || (c*c)>(b*b)+(a*a))
+    {
+        printf("TRIANGULO OBTUSANGULO\n");
+    }
+    else if((a*a)<(b*b)+(c*c) || (b*b)<(a*a)+(c*c) || (c*c)<(b*b)+(a*a))
+    {
+        printf("TRIANGULO ACUTANGULO\n");
+    }
+    if(a==b && a==c)
+    {
+        printf("TRIANGULO EQUILATERO\n");
+    }
+    if((a==b && a!=c) || (b==c && b!=a) || (c==a && c!=b))
+    {
+        printf("TRIANGULO ISOSCELES\n");
+    }
+    return 0;
+}
